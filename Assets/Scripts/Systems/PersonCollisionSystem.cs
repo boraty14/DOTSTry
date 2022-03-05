912ac228eaa4e3deae0518b63aca5a5a84edd711
boraty <brataysen@gmail.com>
2022-03-05 18:12:05 +0300

    changing colors

--- Assets/Scripts/Systems/PersonCollisionSystem.cs
+++ Assets/Scripts/Systems/PersonCollisionSystem.cs
@@ -1,6 +1,11 @@
+using Components;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Physics;
 using Unity.Physics.Systems;
+using Unity.Rendering;
+using UnityEngine;
+using Random = Unity.Mathematics.Random;
 
 namespace Systems
 {
@@ -8,7 +13,7 @@ namespace Systems
     {
         private BuildPhysicsWorld _buildPhysicsWorld;
         private StepPhysicsWorld _stepPhysicsWorld;
-        
+
         protected override void OnCreate()
         {
             base.OnCreate();
@@ -18,9 +23,41 @@ namespace Systems
 
         struct PersonCollisionJob : ITriggerEventsJob
         {
+            [ReadOnly] public ComponentDataFromEntity<PersonTag> PersonGroup;
+            public ComponentDataFromEntity<URPMaterialPropertyBaseColor> ColorGroup;
+            public float Seed;
+
             public void Execute(TriggerEvent triggerEvent)
             {
+                bool isEntityAPerson = PersonGroup.HasComponent(triggerEvent.EntityA);
+                bool isEntityBPerson = PersonGroup.HasComponent(triggerEvent.EntityB);
+
+                if (!isEntityAPerson || !isEntityBPerson) return;
+                float newRandom = (1f + Seed) + (triggerEvent.BodyIndexA * triggerEvent.BodyIndexB);
+                if (newRandom <= 0.001f)
+                {
+                    Debug.Log(1f + Seed);
+                    Debug.Log(triggerEvent.BodyIndexA * triggerEvent.BodyIndexB);
+                }
+                var random =
+                    new Random((uint)(newRandom));
                 
+                random = ChangeMaterialColor(random,triggerEvent.EntityA);
+                random = ChangeMaterialColor(random,triggerEvent.EntityB);
+            }
+
+            private Random ChangeMaterialColor(Random random, Entity entity)
+            {
+                if (ColorGroup.HasComponent(entity))
+                {
+                    var colorComponent = ColorGroup[entity];
+                    colorComponent.Value.x = random.NextFloat(0f, 1f);
+                    colorComponent.Value.y = random.NextFloat(0f, 1f);
+                    colorComponent.Value.z = random.NextFloat(0f, 1f);
+                    ColorGroup[entity] = colorComponent;
+                }
+
+                return random;
             }
         }
 
@@ -28,7 +65,9 @@ namespace Systems
         {
             Dependency = new PersonCollisionJob()
             {
-
+                PersonGroup = GetComponentDataFromEntity<PersonTag>(true),
+                ColorGroup = GetComponentDataFromEntity<URPMaterialPropertyBaseColor>(),
+                Seed = System.DateTimeOffset.Now.Millisecond
             }.Schedule(_stepPhysicsWorld.Simulation, ref _buildPhysicsWorld.PhysicsWorld, Dependency);
         }
     }
